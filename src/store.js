6ef0bb3834966f5d05a1ec5060a05be3a4594a82
shivansh <shivansh211299@gmail.com>
2022-05-14 13:18:34 +0530

    dat.gui

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
-import create from 'zustand'
+import create from "zustand";
 
 const useStore = create((set, get) => ({
   cartItems: 0,
   addItem: () => set((state) => ({ cartItems: state.cartItems + 1 })),
   removeItem: () => set((state) => ({ cartItems: state.cartItems - 1 })),
-  clearCart: () => set((state) => ({ cartItems: 0 }))
-}))
+  clearCart: () => set((state) => ({ cartItems: 0 })),
+  cartItem: [],
+  addCartItem: (item) =>
+    set((state) => ({ cartItem: [...state.cartItem, item] })),
+}));
 
-export default useStore
+export default useStore;
